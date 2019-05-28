6e928c930476cdb7b458d80064fededfe276a20f
avakrishn <avakrishn@gmail.com>
2019-05-27 23:56:15 -0700

    add module file containing text and src for page load

---
+++ src/javascript/data.mjs
@@ -0,0 +1,37 @@
+'use strict';
+
+export const gallery =[
+    {
+        text1: "Hey",
+        text2: "Let's",
+        text3: "Dance",
+    },
+    {
+        text1: "Give",
+        text2: "Take",
+        text3: "Receive",
+    },
+    {
+        text1: "Experience",
+        text2: "It",
+        text3: "Today",
+    },
+    {
+        text1: "Give",
+        text2: "All",
+        text3: "You Can",
+    },
+    {
+        text1: "Life",
+        text2: "In",
+        text3: "Motion",
+    }
+]
+
+export const src = {
+    panel1: "./src/images/dance.jpg",
+    panel2: "./src/images/give-receive.jpg",
+    panel3: "./src/images/experience-02.jpg",
+    panel4: "./src/images/give-all.jpg",
+    panel5: "./src/images/motion.jpg"
+}
